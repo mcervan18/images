f802e04d4b6051265d3f574d4bc5e88ddf92d91d
Justin <justinmjoyce75@gmail.com>
2019-11-08 16:31:14 -0500

    testing

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 
+
 import os 
-filename = os.path.join(skimage.data_dir, 'moon.png')
+filename = '1_Color.png'
 from skimage import io 
 moon = io.imread(filename)
 
+from skimage.viewer import ImageViewer
+
+viewer = ImageViewer(one)
+viewer.show()
+
+len, wid, rgb = one.shape

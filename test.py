1da0987aced738be052757a97fdfc75282b12ed1
Justin <justinmjoyce75@gmail.com>
2019-11-08 17:44:51 -0500

    segmentation attempts

--- test.py
+++ test.py
@@ -1,18 +1,43 @@
-
+# testing stuff 
+from PIL import Image 
 
 import os 
-filename = '1_Color.png'
+filename = 'dots.png'
 from skimage import io 
-one = io.imread(filename)
+dots = io.imread(filename)
+
+lines = io.imread('2_color.png')
+
 
 from skimage.viewer import ImageViewer
 
-
-
-viewer = ImageViewer(one)
+viewer = ImageViewer(lines)
 viewer.show()
 
-len, wid, rgb = one.shape
+# len, wid, rgb = dots.shape
 
-mask = one < 87 
-one[mask] = 255
+# mask = dots < 87 
+
+## from PIL import Image 
+# img = Image.open('dots.png').convert('LA')
+# img.save('gray.png') 
+
+dots1 = dots[:,:,1]
+lines1 = dots[:,:,1]
+
+im_dots1 = Image.fromarray(dots1)
+im_dots1.show()
+im_lines1 = Image.fromarray(lines)
+im_lines1.show()
+
+from skimage.feature import canny 
+edges = canny(dots1)
+
+im_edges = Image.fromarray(edges)
+im_edges.show()
+
+from scipy import ndimage as ndi 
+fill_dots = ndi.binary_fill_holes(edges)
+
+im_fill = Image.fromarray(fill_dots)
+im_fill.show()
